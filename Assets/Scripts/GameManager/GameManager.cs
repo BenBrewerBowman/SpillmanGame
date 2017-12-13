b0cc425592dabfa5f2b2f496a51e85f19e1e5bb0
Joseph Lee <jlee@spillman.com>
2017-12-13 09:56:16 -0700

    Added some scripts

---
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager {
+
+    public enum GameMode
+    {
+        CAR,
+        PLAYER
+    }
+
+    private static GameManager instance = new GameManager();
+    private GameMode gameMode { get; private set; }
+
+    private GameManager() {
+        gameMode = GameMode.PLAYER;
+    }
+
+    public GameManager GetInstance()
+    {
+        return instance;
+    }
+
+    public ToggleGameMode()
+    {
+        if(GameMode == GameMode.CAR)
+        {
+            gameMode = GameMode.PLAYER;
+        }
+    }
+}
